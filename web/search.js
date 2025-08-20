61e23d8f35ed91659c388c1b4b5d22987011196e
Javier Jimenez Shaw <javier.shaw@pix4d.com>
2025-08-20 11:50:55 +0200

    more things

--- web/search.js
+++ web/search.js
@@ -1,13 +1,140 @@
 "use strict";
 
 let g_data = null
+let g_entry = null
+let g_url = null
+let g_fetched_sourcetable = null
+let g_fetched_sourcetable_status = null
 
-function submit_details(form) {
+function split_by_lines(text) {
+    let line_endings = ["\r\n","\n\r","\r","\n"], line_ending = "";
+    for(line_ending of line_endings){
+        if (text.indexOf(line_ending)!=-1){
+            break;
+        }
+    }
+    return text.split(line_ending);
+}
+
+function get_str_line_from_server(streams_from_server, mountpoint) {
+    if (!streams_from_server)
+        return null;
+    const lines = split_by_lines(streams_from_server);
+    for (const line of lines) {
+        const splitted = line.split(";")
+        if (splitted.length > 2 && splitted[0] == "STR" && splitted[1] == mountpoint)
+            return splitted
+    }
+    return null;
+}
+
+function normalize_lon(lon) {
+    while (lon > 180)
+        lon -= 360
+    while (lon < -180)
+        lon += 360
+    return lon
+}
+
+
+function normalize_bbox(bbox) {
+    return [normalize_lon(bbox[0]), bbox[1], normalize_lon(bbox[2]), bbox[3]]
+}
+
+
+function point_in_bbox(point_lat, point_lon, bbox) {
+    if (point_lat > bbox[3] || point_lat < bbox[1])
+        return false
+
+    point_lon = normalize_lon(point_lon)
+    bbox = normalize_bbox(bbox)
+    if (bbox[0] > bbox[2]) {
+        // crossing antimeridian
+        return point_lon >= bbox[0] || point_lon <= bbox[2]
+    } else {
+        return point_lon >= bbox[0] && point_lon <= bbox[2]
+    }
+}
+
+function _crss_from_stream(stream, mountpoint, url, server_streams) {
+    const crss = stream["crss"]
+    const stream_filter = stream["filter"]
+    if (stream_filter == "all") {
+        return crss;
+    } else if ("mountpoints" in stream_filter) {
+        if (stream_filter["mountpoints"].includes(mountpoint))
+            return crss;
+    } else {
+        const line = get_str_line_from_server(server_streams, mountpoint)
+        if (!line || line.length < 10) {
+            console.error(`To process this service completely we need the SOURCETABLE from ${url}, that is not accesible`); // should we alert?
+            return null;
+        }
+        const country = line[8];
+        const base_lat = parseFloat(line[9]);
+        const base_lon = normalize_lon(parseFloat(line[10]));
+
+        if ((stream_filter["countries"] ?? []).includes(country))
+            return crss;
+
+        for (const bbox of (stream_filter["lat_lon_bboxes"] ?? [])) {
+            if (point_in_bbox(base_lat, base_lon, bbox))
+                return crss;
+        }
+    }
+    return null;
+}
+
+function filter_crs(
+    entry,
+    url,
+    mountpoint,
+    rover_lat,
+    rover_lon,
+    rover_country=null) {
+
+    function filter_by_rover(crss) {
+        for (const crs of crss) {
+            if ("rover_bbox" in crs) {
+                if (isNaN(rover_lat) || isNaN(rover_lon)) {
+                    console.error("Rover latitude and longitude needed for this configuration");
+                    window.alert("Rover latitude and longitude are needed for this configuration");
+                    return null;
+                }
+                if (point_in_bbox(rover_lat, rover_lon, crs["rover_bbox"]))
+                    return crs;
+            } else if ("rover_countries" in crs) {
+                console.warn("Rover 3 letter country code could be needed for this configuration");
+                window.alert("Rover 3 letter country code could be needed for this configuration");
+                if (rover_country && crs["rover_countries"].includes(rover_country))
+                    return crs;
+            } else {
+                return crs;
+            }
+        }
+        return null;
+    }
+
+    for (const stream of entry["streams"]) {
+        const crss = _crss_from_stream(stream, mountpoint, url, g_fetched_sourcetable.content)
+        if (crss) {
+            const crs = filter_by_rover(crss)
+            if (crs) {
+                return crs
+            }
+        }
+    }
+
+    return null
+}
+
+function update_browser_url_with_params(form) {
     let url = new URL(location);
     function process_key(key, uppercase) {
         let v = form.elements[key].value;
-        if (uppercase)
+        if (uppercase) {
             v = v.toUpperCase()
+        }
         if (v) {
             url.searchParams.set(key, v);
         } else {
@@ -19,18 +146,46 @@ function submit_details(form) {
     process_key('latitude');
     process_key('longitude');
     window.history.replaceState('', '', url.toString());
+
+}
+
+// Event when Filter button is clicked or page is loaded with entry and mountpoint data
+function submit_details(form) {
+    document.querySelector('#crs_content').textContent = '';
+    if (!form) {
+        form = document.querySelector('#details_form');
+    }
+    update_browser_url_with_params(form);
+    let crs = filter_crs(g_entry, g_url,
+        form.elements.mountpoint.value,
+        parseFloat(form.elements.latitude.value),
+        parseFloat(form.elements.longitude.value),
+        form.elements.country.value);
+    if (crs) {
+        if (crs.name)
+        {
+            document.querySelector('#crs_name').textContent = crs.name;
+            document.querySelector('#crs_name').classList.remove("hidden");
+        }
+        if (crs.id) {
+            let auth_code = crs.id.split(':');
+            let link = `https://spatialreference.org/ref/${auth_code[0].toLowerCase()}/${auth_code[1]}/`;
+            document.querySelector('#crs_id').innerHTML = `<a href="${link}" target=_blank>${crs.id}</a>`;
+            document.querySelector('#crs_id').classList.remove("hidden");
+        }
+    } else {
+        document.querySelector('#crs_name').classList.add("hidden");
+        document.querySelector('#crs_id').classList.add("hidden");
+    }
+    document.querySelector('#crs_content').textContent = JSON.stringify(crs, null, 4);
 }
 
 function paramsToDic(location) {
     let url = new URL(location);
-    url.searchParams.set('hh', "asdf");
     let dic = {};
     for (let k of url.searchParams.keys()) {
         dic[k] = url.searchParams.get(k);
     }
-    console.log(url.toString())
-    let a = url.toJSON()
-    console.log(a)
     return dic;
 }
 
@@ -54,16 +209,54 @@ function init_search() {
                     let url = data.entries[i].urls[j];
                     if (url == "http://" + params.url + ":" + params.port) {
                         entry = data.entries[i];
+                        g_entry = entry;
+                        g_url = url;
                         document.querySelector('#entry_content').textContent = JSON.stringify(entry, null, 4);
                         document.querySelector('#entry_name').textContent = entry.name;
                         document.querySelector('#entry_description').textContent = entry.description;
                         document.querySelector('#entry_ref').innerHTML = `<a href="${entry.reference.url}" target="_blank">${entry.reference.url}</a>`;
+
+                        if (entry) {
+                            ['mountpoint', 'country', 'latitude', 'longitude'].forEach(e => fill_value(e));
+                            document.querySelector('#url_ok').classList.remove("hidden");
+                            if (document.querySelector('#mountpoint').textContent)
+                                document.querySelector('#crs_content').textContent = "... loading";
+                        } else {
+                            document.querySelector('#url_not_ok').classList.remove("hidden");
+                        }
+
+                        // We do this request via this service to avoid CORS problems.
+                        // See that bumblebee is currently located in AWS, and some countries/services
+                        // may lock (or allow) those URLs
+                        fetch("https://api.dev.webgis.pix4d.com/bumblebee/v0/ntrip/sourcetable", {
+                            method: "POST",
+                            headers: {
+                                "accept": "application/json",
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({ "url": url })
+                        })
+                        .then((response) => {
+                            if (response.ok) {
+                                return response.json();
+                            }
+                            console.log(response)
+                            throw new Error('Something went wrong getting the soucetable for ' + url);
+                        })
+                        .then((json) => {
+                            console.log("release:", json.release);
+                            document.querySelector('#sourcetable_content').textContent = json.content;
+                            g_fetched_sourcetable = json;
+                            if (document.querySelector('#mountpoint').value.length)
+                                submit_details();
+                        })
+                        .catch((error) => {
+                            console.log(error)
+                            document.querySelector('#crs_content').textContent = "--- error ---"
+                        });
                         break;
                     }
                 }
-            }
-            if (entry) {
-                ['mountpoint', 'country', 'latitude', 'longitude'].forEach(e => fill_value(e))
             }
         });
     }
---
+++ web/test_search.js
@@ -0,0 +1,69 @@
+"use strict"
+let list_checked = [];
+let g_data = null;
+
+function run_checks() {
+    let counter = 0;
+    for (let test of g_data) {
+        for (let check of test.checks) {
+            let iframe = document.querySelector(`#iframe_${counter}`).contentWindow.document;
+            let a = iframe.querySelector('#crs_content');
+            if (iframe.querySelector('#crs_content').textContent.length > 0){
+                let res = true;
+                if (check.result.name) {
+                    let name = iframe.querySelector('#crs_name');
+                    if (name.innerText != check.result.name)
+                        res = false;
+                }
+                if (check.result.id) {
+                    let id = iframe.querySelector('#crs_id');
+                    if (!id.innerHTML.includes(check.result.id))
+                        res = false;
+                }
+                list_checked[counter] = res;
+                document.querySelector(`#res_${counter}`).innerHTML = res ? '&#x2705;' : '&#x274C;';
+            }
+            counter += 1;
+        }
+    }
+
+    if (!list_checked.every(v => v !== ''))
+        setTimeout(run_checks, 1000);
+}
+
+
+function init_test_search() {
+    fetch('./test_search.json', {
+        method: "GET",
+    })
+    .then(response => response.json())
+    .then(data => {
+        g_data = data;
+        let counter = 0;
+        for (let test of data) {
+            for (let check of test.checks) {
+                let iframe = document.createElement('iframe');
+                let src = `./search.html?url=${test.url}&port=${test.port}&mountpoint=${check.mountpoint}`;
+                for (const item of ['latitude', 'longitude', 'country']) {
+                    if (check[item] !== undefined)
+                        src += `&${item}=${check[item]}`
+                }
+                iframe.src = src;
+                iframe.width = "50%";
+                iframe.height="5";
+                iframe.id= `iframe_${counter}`
+                iframe.title = check.description;
+                const desc = document.createElement('div');
+                desc.innerHTML = `<a href="${src}" target=_blank>${check.description}</a>`;
+                const res = document.createElement('div');
+                res.id = "res_" + counter;
+                res.innerHTML = '... &quest;'
+                document.getElementById('iframes_list').appendChild(desc);
+                document.getElementById('iframes_list').appendChild(res);
+                document.getElementById('iframes_list').appendChild(iframe);
+                counter += 1;
+            }
+        }
+        list_checked = new Array(counter).fill('');
+    })
+}
